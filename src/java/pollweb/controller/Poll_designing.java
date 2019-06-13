1dea1aa64e9c82498a25afc8631f1dd8236429e0
Aurelien <lebrun.aurel@gmail.com>
2019-06-13 10:45:06 +0200

    New servlet designing

--- src/java/pollweb/controller/Poll_designing.java
+++ src/java/pollweb/controller/Poll_designing.java
@@ -1,4 +1,4 @@
-package com.octest.servlets;
+package pollweb.controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -10,14 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class servlet_home
  */
-@WebServlet("/servlet_poll_designing")
-public class servlet_poll_designing extends HttpServlet {
+@WebServlet("/Poll_designing")
+public class Poll_designing extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public servlet_poll_designing() {
+    public Poll_designing() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,6 +26,8 @@ public class servlet_poll_designing extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                String log = "Login";
+                request.setAttribute("log", log);
 		this.getServletContext().getRequestDispatcher("/WEB-INF/JSP/poll_designing.jsp").forward(request, response);
 	}
 
@@ -33,6 +35,17 @@ public class servlet_poll_designing extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            
+                String poll_title = request.getParameter("poll_title");
+                request.setAttribute("poll_title", poll_title);
+                String opening_text = request.getParameter("opening_text");
+                request.setAttribute("opening_text", opening_text);
+                String closing_text = request.getParameter("closing_text");
+                request.setAttribute("closing_text", closing_text);
+                String poll_reserved = request.getParameter("reserved");
+                request.setAttribute("poll_reserved", poll_reserved);
+            
+            
 		this.getServletContext().getRequestDispatcher("/WEB-INF/JSP/poll_designing.jsp").forward(request, response);
 	}
 
